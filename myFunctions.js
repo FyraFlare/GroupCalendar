6306948b2c7c30a4a99630942bc4a9444676584f
Sean Adams <sadams15@email.arizona.edu>
2016-10-07 12:30:29 -0700

    Added Underscores Again

--- myFunctions.js
+++ myFunctions.js
@@ -84,7 +84,7 @@ function login(){
     var args = {submit: 'Login', username: user, pass: pass};
     $http.post("login.php", args).then(function(data){
         if(data == 'good'){
-            window.location.assign("http://localhost/GroupCalendar/main.html");
+            window.location.assign("http://localhost/Group_Calendar/main.html");
         }
         else{
             document.getElementById('error').innerHTML = data;
@@ -96,7 +96,7 @@ function logout(){
     var args = {want: 'logout'};
     $http.post("wanted.php", args).then(function(data){
         console.log(data);
-        window.location.assign("http://localhost/GroupCalendar/login.html");
+        window.location.assign("http://localhost/Group_Calendar/login.html");
     });
 }
 
@@ -110,7 +110,7 @@ function register(){
     var args = {submit: 'Register', username: document.getElementById('username').value, pass: pass1};
     $http.post("login.php", args).then(function(data){
         if(data == 'good'){
-            window.location.assign("http://localhost/GroupCalendar/login.html");
+            window.location.assign("http://localhost/Group_Calendar/login.html");
         }
         else{
             document.getElementById('error').innerHTML = data;
